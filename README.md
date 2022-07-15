a3ce1f7e208133a0fe7e2409a580f41028070466
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-07-14 21:40:55 -0600

    update js

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Javascript-Password-Creator
+
+Create a site that will generate a password
+ -have a certain number of characters
+ -select which type of characters(uppercase, lowercase, numbers, and special characters)
+ -after ask if user would like to create another password
--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,11 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+//Need to have a question prompt for which characters the user wants to include(upper/lowercase, numbers, special characters)
+//Need password to be a certain length
+//Use Math for random generation of characters
+//Use an array to hold the characters
+//Need to ask after password generation if user wants to create a new password
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
